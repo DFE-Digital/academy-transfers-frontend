bb3eb022dde9d85cf17417d1e65c6ff5cc97f9e7
Rob Nichols <rob@undervale.co.uk>
2021-01-18 15:04:01 +0000

    Add tabbed layout to dashboard
    - Adds transfers, conversions, significant change, and concerns tabs
    - Moves current content to transfers tabs
    - Add a new partial for each tab
    - Add helper methods to create links to tabs and render partials in tab div

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,18 @@ module ApplicationHelper
   def page_header_label
     content_for?(:page_header) ? content_for(:page_header) : t("default_page_header")
   end
+
+  def tab_link(name, selected: false)
+    classes = ["govuk-tabs__list-item"]
+    classes << "govuk-tabs__list-item--selected" if selected
+
+    link = link_to(t(".tab_headers.#{name}"), "##{name}", class: "govuk-tabs__tab", id: "tab_#{name}"
+    )
+
+    content_tag(:li, link, class: classes)
+  end
+
+  def render_as_tab(name, *args)
+    content_tag :div, render(name.to_s *args), id: name, class: "govuk-tabs__panel"
+  end
 end
